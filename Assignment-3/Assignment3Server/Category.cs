aa5d248feae173047322dda9e3fb15b1c865ca5f
Pozsgai Alex <jatekalex@gmail.com>
2023-10-16 15:01:57 +0200

    Refactor classes into different files

---
+++ Assignment-3/Assignment3Server/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace categoryNS
+{
+    public class Category
+    {
+        [JsonPropertyName("cid")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
