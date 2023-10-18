ab88d188a1fb03b0280d5db3f86efd5560534b61
Pozsgai Alex <jatekalex@gmail.com>
2023-10-18 15:23:34 +0200

    First 7 tests are done

--- Assignment-4/DataLayer/DataService.cs
+++ Assignment-4/DataLayer/DataService.cs
@@ -2,4 +2,44 @@
 
 public class DataService
 {
+    public IList<Category> GetCategories()
+    {
+        var db = new NorthwindContex();
+        return db.Categories.ToList();
+    }
+    public Category? GetCategory(int categoryId)
+    {
+        var db = new NorthwindContex();
+        return db.Categories.FirstOrDefault(x => x.Id == categoryId);
+    }
+    public Category CreateCategory(string name, string description)
+    {
+        var db = new NorthwindContex();
+        var id = db.Categories.Max(x => x.Id) + 1;
+        var category = new Category
+        {
+            Id = id,
+            Name = name,
+            Description = description
+        };
+        db.Add(category);
+        db.SaveChanges();
+        return category;
+    }
+    public bool DeleteCategory(Category category)
+    {
+        return DeleteCategory(category.Id);
+    }
+    public bool DeleteCategory(int categoryId)
+    {
+        var db = new NorthwindContex();
+        var category = db.Categories.FirstOrDefault(x => x.Id == categoryId);
+        if (category != null)
+        {
+            db.Categories.Remove(category);
+            //db.Remove(category);
+            return db.SaveChanges() > 0;
+        }
+        return false;
+    }
 }
