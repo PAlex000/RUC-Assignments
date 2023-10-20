dc7ad86c5caf5cea105529a3effd6634bd20e8f3
Pozsgai Alex <jatekalex@gmail.com>
2023-10-20 14:24:44 +0200

    Remove toString from Category

--- Assignment-4/DataLayer/Category.cs
+++ Assignment-4/DataLayer/Category.cs
@@ -4,9 +4,4 @@ public class Category
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-
-    public override string ToString()
-    {
-        return $"{Id}, {Name}, {Description}";
-    }
 }
