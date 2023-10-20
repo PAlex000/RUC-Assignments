cb0e348602a2d1c01af9d601a5f2adc4b4353bf1
Pozsgai Alex <jatekalex@gmail.com>
2023-10-20 15:08:26 +0200

    Add Order, OrderDetails, solve Order tests

---
+++ Assignment-4/DataLayer/OrderDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class OrderDetails
+    {
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public double UnitPrice { get; set; }
+        public double Quantity { get; set; }
+        public double Discount { get; set; }
+    }
+}
