7e0da9568c9e4595628207b0ddd634e9e67caae1
meii181 <myangeldankouzo00@gmail.com>
2023-10-20 20:01:30 +0200

    code for assignment 4

---
+++ Assignment4Part1/DataLayer/DataService.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DataLayer;
+
+public class DataService
+{
+    public IList<Category> GetCategories()
+    {
+        var db = new NorthwindContex();
+    }
+}
