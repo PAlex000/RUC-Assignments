66b49c55479289a7ff9a8e963b7af0b3cdefebe7
Pozsgai Alex <jatekalex@gmail.com>
2023-10-20 14:25:22 +0200

    Complete 2 more tests using linq

--- Assignment-4/DataLayer/DataService.cs
+++ Assignment-4/DataLayer/DataService.cs
@@ -55,10 +55,45 @@ public class DataService
         }
         return false;
     }
-    public Product? GetProduct(int productId)
+    public ProductWithCategoryName GetProduct(int productId)
     {
         var db = new NorthwindContex();
-        Product product = db.Products.First(x => x.Id == productId);
-        return product;
+        Product product = db.Products.FirstOrDefault(x => x.Id == productId);
+        Category category = db.Categories.FirstOrDefault(x => x.Id == product.CategoryId);
+        ProductWithCategoryName result = new ProductWithCategoryName
+        {
+            Id = product.Id,
+            Name = product.Name,
+            UnitPrice = product.UnitPrice,
+            QuantityPerUnit = product.QuantityPerUnit,
+            UnitsInStock = product.UnitsInStock,
+            CategoryName = category.Name
+        };
+        return result;
+    }
+    public List<ProductWithCategoryName> GetProductByCategory(int categoryId)
+    {
+        var db = new NorthwindContex();
+        return db.Products
+            .Where(x => x.CategoryId == categoryId)
+            .Select(x => new ProductWithCategoryName
+            {
+                Id = x.Id,
+                Name = x.Name,
+                UnitPrice = x.UnitPrice,
+                QuantityPerUnit = x.QuantityPerUnit,
+                UnitsInStock = x.UnitsInStock,
+                CategoryName = x.category.Name
+            })
+            .ToList();
+    }
+    public List<ProductAndCategoryNames> GetProductByName(string productName)
+    {
+        var db = new NorthwindContex();
+
+        return db.Products
+            .Where(x => x.Name.ToLower().Contains(productName.ToLower()))
+            .Select(x => new ProductAndCategoryNames { ProductName = x.Name, CategoryName = x.category.Name })
+            .ToList();
     }
 }
