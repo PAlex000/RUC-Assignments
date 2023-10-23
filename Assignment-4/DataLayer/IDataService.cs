21cc08659e5816c66fe6996de95c283e9daf175c
Pozsgai Alex <jatekalex@gmail.com>
2023-10-23 09:38:22 +0200

    Add webserver, refactor

---
+++ Assignment-4/DataLayer/IDataService.cs
@@ -0,0 +1,22 @@
+﻿using DataLayer.Models;
+using System.Collections.Generic;
+
+namespace DataLayer
+{
+    public interface IDataService
+    {
+        IList<Category> GetCategories();
+        Category? GetCategory(int categoryId);
+        Category CreateCategory(string name, string description);
+        bool DeleteCategory(Category category);
+        bool DeleteCategory(int categoryId);
+        bool UpdateCategory(int id, string name, string description);
+        ProductWithCategoryName GetProduct(int productId);
+        List<ProductWithCategoryName> GetProductByCategory(int categoryId);
+        List<ProductAndCategoryNames> GetProductByName(string productName);
+        Order GetOrder(int orderId);
+        List<Order> GetOrders();
+        List<OrderDetails> GetOrderDetailsByOrderId(int orderId);
+        List<OrderDetails> GetOrderDetailsByProductId(int productId);
+    }
+}
